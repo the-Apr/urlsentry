221f3a15ce5d4f8aebe31aa01e80d8e6e237fe7e
Praise <p.o.adebayo218@gmail.com>
2023-08-12 16:50:16 +0100

    added FAQ components

---
+++ src/components/FaqItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid grid-cols-4">
+    <div class="col-span-1">^</div>
+    <div class="col-span-3">
+      <h1 class="font-bold text-xl">{{title}}</h1>
+      <p>{{ body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import phisingDoc from './phishingDoc.vue'
+export default {
+  components: {
+    phisingDoc,
+  }
+}
+</script>
